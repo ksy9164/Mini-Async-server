7f932acfdd855bb20da69b0a16a996d94c4542e5
Seongyoung-Kang <ksy9164@naver.com>
2019-02-08 14:59:47 +0900

    Simple async aio & running sub routine

--- server.py
+++ server.py
@@ -1,16 +1,30 @@
 from aiohttp import web
+import asyncio
+
 routes = web.RouteTableDef()
 
-@routes.get('/')
-async def hello(request):
-    print("!!!")
-    return web.Response(text="Hello, world")
+log = 0
 
-@routes.get('/test')
-async def hello(request):
-    print("@@@")
-    return web.Response(text="Hello, world")
+@routes.get('/')
+async def normal_handler(request):
+    global log
+    print ("The log is ",log)
+    msg = "Hellow the log is "
+    num = str(log)
+    msg = msg + num
+    return web.Response(text=msg)
+
+async def display_date():
+    while True:
+        global log
+        log += 1
+        await asyncio.sleep(1)
 
 app = web.Application()
 app.add_routes(routes)
+
+loop = asyncio.get_event_loop()
+
+future = loop.create_task(display_date())
+
 web.run_app(app)
